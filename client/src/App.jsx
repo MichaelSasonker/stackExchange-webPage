bb06ddbd057569ddfa1a9d92f2ebb7ef9846bd6f
Zehava-Bar <zehava.bargil@gmail.com>
2021-04-27 16:21:04 +0300

    react

---
+++ client/src/App.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import Navbar from './Components/Navbar/Navbar';
+import Home from './Components/Home/Home';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Search from './Components/Search/Search';
+import WishList from './Components/WishList/WishList';
+import './Components/Style/Style.css';
+
+
+export default function App() {
+  return (
+        <div>
+          <Router>
+         <Navbar/>
+         <Switch>
+           <Route path='/' exact component=
+           {Home}/>
+           <Route path='/search' 
+           component=
+           {Search} />
+          <Route path='/wishList' component=
+           {WishList} />
+         </Switch>
+         </Router>     
+    </div>
+  )
+}
