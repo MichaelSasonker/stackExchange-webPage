ac0d7af98afc2c26f92c8b83d9de884605dc5f0b
Zehava-Bar <zehava.bargil@gmail.com>
2021-04-27 23:04:21 +0300

    react

---
+++ client/src/Components/Favourites/SaveF.jsx
@@ -0,0 +1,39 @@
+import React, { useEffect, useState } from 'react';
+import './Favourites.css';
+// import  { BrowserRouter, Switch, Route } from 'react-router-dom';
+
+export default function SaveF() {
+    const [favourites, setFavourites] = useState([]);
+    const addTofavourites = (item) => {
+        // let isExists = false;
+        // favourites.find((favorite) => {
+        //   if (favorite.id === saved.id) {
+        //     isExists = true;
+        //     alert(“already saved”);
+        //   }
+        // });
+        // if (!isExists) {
+          setFavourites([...favourites, item]);
+          alert(`Saved to list`);
+          console.log(favourites);
+        // }
+      };
+      const deleteFromfavourites = (item) => {
+        setFavourites(
+          favourites.filter((favourite) => favourite.id !== item.id)
+        );
+        return(
+          <div className='SAVED_CONTAINER'>
+              <div className='FAVOURITES'>
+                  {/* saved items mapped - later*/}
+                  onClick={deleteFromfavourites}
+                  {/* buttonText=DELETE' */}
+              </div>
+          </div>
+          )
+      };
+      
+}
+
+
+  
--- client/src/Components/HomePage/HomePage.jsx
+++ client/src/Components/HomePage/HomePage.jsx
@@ -1,31 +1,31 @@
 import React from 'react'
 import { Button } from '../Button/Button';
-import { GiMusicalScore } from 'react-icons/gi';
-import { GiBlackBook } from 'react-icons/gi';
+// import { GiMusicalScore } from 'react-icons/gi';
+
 
 function HomePage() {
     return (
         <div className='home-container'>
-            <video src=
+            {/* <video src=
             "week--project\public\music\song.mp3"
-            autoPlay loop muted />
-            <h2 className='home-title1'>BOOK LIBRARY</h2>
-            <p>Knowlege is power -Keep reading...</p>
+            autoPlay loop muted /> */}
+            <h2 className='home-title1'>STACK EXCHANGE</h2>
+            <p>The important thing is not to stop quesioning... </p>
             <div className='home-btns'>
             <Button
              className='btns'
              buttonStyle='btn--outline'
              buttonSize='btn--large'
              >
-              GET STARTED <GiMusicalScore/>  
+              START ASK  
             </Button>
-            <Button
+            {/* <Button
              className='btns'
              buttonStyle='btn--primary'
              buttonSize='btn--large'
              >
-              COOSE a BOOK <GiBlackBook/>  
-            </Button>
+                
+            </Button> */}
             </div>
         </div>
     )
